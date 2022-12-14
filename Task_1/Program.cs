a039225f0d073c562fd1fa9cdec80632252ed91c
svetlana <masterica75@mail.ru>
2022-12-14 13:08:48 +0700

    Added Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int namber = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Количество цифр в числе = {GetCount(namber)}");
+
+int GetCount(int namTemp)
+{
+    int count = 0;
+    while (namTemp > 0)
+    {
+        namTemp /= 10;
+        count++;
+    }
+    return count;
+}
